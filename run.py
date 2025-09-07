bd86a573759067e071d32334fc6beb404eac43e1
Asia Barbato <asiabarbato@Asias-MacBook-Pro.local>
2025-09-07 15:57:02 -0400

    Fix widget.js path using app.static_folder and correct typo

--- run.py
+++ run.py
@@ -6,13 +6,14 @@ from app.services.matting import bp as matting_bp
 from app.services.render import bp as render_bp
 from pathlib import Path
 
-base_dir = Path(__file__).resolve().parent
+base_dir = Path(__file__).resolve().parent.parent
 app = create_app()
+app.static_folder = str(base_dir / "app/static")  # Explicitly set to match project structure
 
 @app.get("/widget.js")
 def widget_js():
-    print(f"Attemping to serve widget.js from {base_dir / 'app/static/widget.js'}")
-    return send_from_directory("app/static", "widget.js", mimetype="application/javascript")
+    print(f"Attempting to serve widget.js from {app.static_folder}/widget.js")
+    return send_from_directory(app.static_folder, "widget.js", mimetype="application/javascript")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
